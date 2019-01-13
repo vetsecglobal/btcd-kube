40519a7acb53f1648e0199ebdd01ac4fb8305773
kevin <kjwilde@hotmail.com>
2019-01-12 21:15:54 -0600

    Creating shared volume so that lnd pod has access to certs.

--- start-btcd.sh
+++ start-btcd.sh
@@ -51,8 +51,10 @@ PARAMS=$(echo \
     "--rpcpass=$RPCPASS" \
     "--datadir=/data" \
     "--logdir=/data" \
-    "--rpccert=/shared/rpc/rpc.cert" \
-    "--rpckey=/shared/rpc/rpc.key" \
+    "--rpccert=/rpc/rpc.cert" \
+    "--rpckey=/rpc/rpc.key" \
+#    "--rpccert=/shared/rpc/rpc.cert" \
+#    "--rpckey=/shared/rpc/rpc.key" \
     "--rpclisten=0.0.0.0" \
     "--txindex"
 )
