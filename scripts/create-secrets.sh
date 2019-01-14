27665b930ba05af289dd252a02907e0f0310e2c1
kevin <kjwilde@hotmail.com>
2019-01-14 16:24:11 -0600

    Creating shared volume so that lnd pod has access to certs.

--- scripts/create-secrets.sh
+++ scripts/create-secrets.sh
@@ -9,7 +9,7 @@ miningAddress=`echo $3 | base64`
 
 echo "encrypted miningAddress: ${miningAddress}"
 
-cat ./secrets.yml | sed "s/\X_MINING_ADDRESS_X/$miningAddress/" | kubectl --context=${context} --namespace ${namespace} create -f -
+cat ./secrets.yml | sed "s/\X_MINING_ADDRESS_X/${miningAddress}/" | kubectl --context=${context} --namespace ${namespace} create -f -
 
 
 #./create-secrets.sh minikube lightning-kube rcbBivXApy8g2HvSru5VyaWF3mn3Ewwmjr
