40baef8f82fa8b31587ef5785894dbd672610882
kevin <kjwilde@hotmail.com>
2019-01-15 19:24:24 -0600

    Creating shared volume so that lnd pod has access to certs.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 
 # Set the mining flag only if address is non empty.
-if [[ -n "${MINING_ADDRESS}" ] && ["${MINING_ADDRESS}" != "empty" ]]
+if [ -z "${MINING_ADDRESS}" ] && [ "${MINING_ADDRESS}" != "empty" ]
 then
     PARAMS="$PARAMS --miningaddr=$MINING_ADDRESS"
 fi
