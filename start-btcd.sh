72e34857c2964972c157f9f9bf3c87d41b64c55d
kevin <kjwilde@hotmail.com>
2019-01-17 15:22:38 -0600

    Attempting to fix bad cert issue that is preventing lnd node from communicating with btcd.

--- start-btcd.sh
+++ start-btcd.sh
@@ -71,7 +71,7 @@ PARAMS=$(echo \
 
 
 # Set the mining flag only if address is non empty.
-if [ -z "${MINING_ADDRESS}" ] && [ "${MINING_ADDRESS}" != "empty" ]
+if [ ! -z "${MINING_ADDRESS}" ] && [ "${MINING_ADDRESS}" != "empty" ]
 then
     PARAMS="$PARAMS --miningaddr=$MINING_ADDRESS"
 fi
