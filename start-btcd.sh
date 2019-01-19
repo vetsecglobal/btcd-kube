776a79ffded2c8f168d85c767d379565bd103284
kevin <kjwilde@hotmail.com>
2019-01-18 19:21:22 -0600

    Attempting to fix bad cert issue that is preventing lnd node from communicating with btcd.

--- start-btcd.sh
+++ start-btcd.sh
@@ -54,7 +54,7 @@ PARAMS=$(echo \
     "--logdir=/mnt/lk/shared/data" \
     "--rpccert=/mnt/lk/shared/rpc/rpc.cert" \
     "--rpckey=/mnt/lk/shared/rpc/rpc.key" \
-    "--rpclisten=0.0.0.0" \
+    "--rpclisten=127.0.0.1" \
     "--txindex"
 )
 
