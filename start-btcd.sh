3abfc92ed3addfa352106b89b6e5a8240644f519
kevin <kjwilde@hotmail.com>
2019-01-20 21:16:23 -0600

    Attempting to fix bad cert issue that is preventing lnd node from communicating with btcd.

--- start-btcd.sh
+++ start-btcd.sh
@@ -78,7 +78,7 @@ btcdServiceIp=`ping ${btcdHostName} -c1 | head -1 | grep -Eo '[0-9.]{4,}'`
 
 echo "btcdServiceIp: ${btcdServiceIp}"
 
-/bin/gencerts --host="*" --host="${btcdServiceIp}" --directory="/mnt/lk/shared/rpc" --force
+/bin/gencerts --host="*" --host="${btcdServiceIp}" --host="${btcdHostName}" --directory="/mnt/lk/shared/rpc" --force
 
 # Print command and start bitcoin node.
 echo "Command: btcd $PARAMS"
