d7683758ab73407f4962cf171a331487308372e1
kevin <kjwilde@hotmail.com>
2019-01-23 21:59:56 -0600

    Putting each instance into it's own namespace by network.

--- deploy-helm.sh
+++ deploy-helm.sh
@@ -65,7 +65,7 @@ cd ./scripts
 ./delete-pv.sh ${context} ${namespace} lightning-kube-btcd${networkSuffix}
 ./create-pv.sh  ${context} ${namespace} lightning-kube-btcd${networkSuffix}
 
-helm ${kubeContextArg} ${namespaceArg} install -n lightning-kube-btcd${networkSuffix} --set database=${database} ${namespaceValueArg} ${serviceTypeArg} ${nodePortArg} ${networkArg} --set image.tag=${imageTag} charts/lightning-kube-btcd
+helm "${kubeContextArg}" "${namespaceArg}" install -n lightning-kube-btcd${networkSuffix} --set database=${database} ${namespaceValueArg} ${serviceTypeArg} ${nodePortArg} ${networkArg} --set image.tag=${imageTag} charts/lightning-kube-btcd
 
 
 if [ $? -eq 0 ]
