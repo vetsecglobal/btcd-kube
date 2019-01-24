fb6a03b5fa39a26bd8affff081f11401a34ddb88
kevin <kjwilde@hotmail.com>
2019-01-23 21:19:15 -0600

    Putting each instance into it's own namespace by network.

--- scripts/create-pv.sh
+++ scripts/create-pv.sh
@@ -5,7 +5,11 @@ namespace=$2
 name=$3
 
 
-kubectl create --context=${context} --namespace ${namespace} -f ./lightning-kube-pv.yaml
-kubectl create --context=${context} --namespace ${namespace} -f ./lightning-kube-pvc.yaml
+#kubectl create --context=${context} --namespace ${namespace} -f ./lightning-kube-pv.yaml
+#kubectl create --context=${context} --namespace ${namespace} -f ./lightning-kube-pvc.yaml
+
+
+cat ./lightning-kube-pv.yaml | sed "s/\X_NAME_X/${name}/" | kubectl --context=${context} --namespace ${namespace} create -f -
+cat ./lightning-kube-pvc.yaml | sed "s/\X_NAME_X/${name}/" | kubectl --context=${context} --namespace ${namespace} create -f -
 
 #./create-pv.sh minikube lightning-kube
