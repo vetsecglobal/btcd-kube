8b37bcd3ca4367e2fbc43838d218f9843ed2157b
kevin <kjwilde@hotmail.com>
2019-01-29 15:20:42 -0600

    Putting each instance into it's own namespace by network.

--- scripts/create-pv.sh
+++ scripts/create-pv.sh
@@ -5,6 +5,8 @@ namespace=$2
 name=$3
 networkSuffix=$4
 
+echo "create-pv.sh"
+
 echo "context: ${context}"
 echo "namespace: ${namespace}"
 echo "name: ${name}"
@@ -19,8 +21,20 @@ fi
 #kubectl create --context=${context} --namespace ${namespace} -f ./lightning-kube-pv.yaml
 #kubectl create --context=${context} --namespace ${namespace} -f ./lightning-kube-pvc.yaml
 
+echo "debug1"
+kubectl --namespace ${namespace} get pv lightning-kube-pvc${networkSuffix}
+kubectl --namespace ${namespace} get pvc lightning-kube-pvc${networkSuffix}
 
 cat ./lightning-kube-pv.yaml | sed "s/\X_NETWORK_SUFFIX_X/${networkSuffix}/" | kubectl ${kubeContextArg} --namespace ${namespace} create -f -
+
+echo "debug2"
+kubectl --namespace ${namespace} get pv lightning-kube-pvc${networkSuffix}
+kubectl --namespace ${namespace} get pvc lightning-kube-pvc${networkSuffix}
+
 cat ./lightning-kube-pvc.yaml | sed "s/\X_NETWORK_SUFFIX_X/${networkSuffix}/" | kubectl ${kubeContextArg} --namespace ${namespace} create -f -
+
+echo "debug3"
+kubectl --namespace ${namespace} get pv lightning-kube-pvc${networkSuffix}
+kubectl --namespace ${namespace} get pvc lightning-kube-pvc${networkSuffix}
 
 #./create-pv.sh minikube lightning-kube
--- scripts/delete-pv.sh
+++ scripts/delete-pv.sh
@@ -5,6 +5,8 @@ namespace=$2
 name=$3
 networkSuffix=$4
 
+echo "delete-pv.sh"
+
 echo "context: ${context}"
 echo "namespace: ${namespace}"
 echo "name: ${name}"
@@ -19,7 +21,20 @@ fi
 #kubectl delete --context=${context} --namespace ${namespace} -f ./lightning-kube-pv.yaml
 #kubectl delete --context=${context} --namespace ${namespace} -f ./lightning-kube-pvc.yaml
 
+echo "debug1"
+kubectl --namespace ${namespace} get pv lightning-kube-pvc${networkSuffix}
+kubectl --namespace ${namespace} get pvc lightning-kube-pvc${networkSuffix}
+
 cat ./lightning-kube-pv.yaml | sed "s/\X_NETWORK_SUFFIX_X/${networkSuffix}/" | kubectl ${kubeContextArg} --namespace ${namespace} delete -f -
+
+echo "debug2"
+kubectl --namespace ${namespace} get pv lightning-kube-pvc${networkSuffix}
+kubectl --namespace ${namespace} get pvc lightning-kube-pvc${networkSuffix}
+
 cat ./lightning-kube-pvc.yaml | sed "s/\X_NETWORK_SUFFIX_X/${networkSuffix}/" | kubectl ${kubeContextArg} --namespace ${namespace} delete -f -
+
+echo "debug3"
+kubectl --namespace ${namespace} get pv lightning-kube-pvc${networkSuffix}
+kubectl --namespace ${namespace} get pvc lightning-kube-pvc${networkSuffix}
 
 #./delete-pv.sh minikube lightning-kube
