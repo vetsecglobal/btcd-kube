9a5ddd7e0364c34ee97e5d8a20940041b3ad4a85
kevin <kjwilde@hotmail.com>
2019-02-23 15:24:45 -0600

    Added lightning-kube-mainnet environment.

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jx create env -n lightning-kube-mainnet -l lightning-kube-mainnet --namespace lightning-kube-mainnet --git-ref https://github.com/kevinstl/environment-lightning-kube-mainnet --promotion Manual
+jx create env -n lightning-kube-mainnet -l lightning-kube-mainnet --namespace lightning-kube-mainnet --promotion Manual
