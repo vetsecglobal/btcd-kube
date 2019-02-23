1cfba1e7972733ccbe8af6364236e224fc2f9d84
kevin <kjwilde@hotmail.com>
2019-02-23 16:00:23 -0600

    Added lightning-kube-simnet and lightning-kube-testnet environments.

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+jx create env -n lightning-kube-simnet -l lightning-kube-simnet --namespace lightning-kube-simnet --promotion Manual
+jx create env -n lightning-kube-testnet -l lightning-kube-testnet --namespace lightning-kube-testnet --promotion Manual
 jx create env -n lightning-kube-mainnet -l lightning-kube-mainnet --namespace lightning-kube-mainnet --promotion Manual
